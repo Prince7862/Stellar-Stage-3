ab08aef1dbda3bbbadb42a905d83ce330a92c75a
Prince Ali Jeewani <princealijeewani@Princes-Air.attlocal.net>
2021-06-23 20:08:34 -0500

    Update to StarMap Screen

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -1,80 +1,84 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
-import  WebView  from 'react-native-webview';
+import { Text, View, TextInput, StyleSheet, Platform, StatusBar, SafeAreaView, ImageBackgrounda, ImageBackground } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default class StarMap extends Component {
-  constructor(){
-    super()
-    this.state = {
-      longitude:{},
-      latitude:{}
+export default class StarMapScreen extends Component {
+    constructor() {
+        super()
+        this.state = {
+            longitude: '',
+            latitude: ''
+        }
     }
-   
-    
-  }
-  render() {
-    return (
-      
-      <View style={styles.background}>
-       <SafeAreaView style={styles.droidSafeArea} />
-      <Text style={styles.titleText}>Star Map</Text>
-
-      <TextInput
-      style ={styles.input}
-      placeholder = "Enter your Longitude"
-      placeholderTextColor = "#ffff"
-      onChangeText={(text)=>{
-        this.setState({
-          longitude: text
-        })
-      }}
-      />
-
-      <TextInput
-      style ={styles.input}
-      placeholder = "Enter your Latitude"
-      placeholderTextColor = "#ffff"
-      onChangeText={(text)=>{
-        this.setState({
-          latitude: text
-        })
-      }}
-      />
-      <WebView
-      scalesPageToFit={true}
-        source={{ uri: 'https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=false'}}
-      style={{ marginTop: 20, marginBottom: 20 }}
-      />
-      </View>
-    );
-  }
+    render() {
+        const { longitude, latitude } = this.state;
+        const path = `https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&showdate=false&showposition=false`
+        return (
+            <View style={{ flex: 1, backgroundColor: "#1a0023" }}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={{ flex: 0.3, marginTop: 20, alignItems: 'center' }}>
+                    <Text style={styles.titleText}>Star Map</Text>
+                    <TextInput
+                        style={styles.inputStyle}
+                        placeholder="Enter your longitude"
+                        placeholderTextColor="white"
+                        onChangeText={(text) => {
+                            this.setState({
+                                longitude: text
+                            })
+                        }}
+                    />
+                    <TextInput
+                        style={styles.inputStyle}
+                        placeholder="Enter your latitude"
+                        placeholderTextColor="white"
+                        onChangeText={(text) => {
+                            this.setState({
+                                latitude: text
+                            })
+                        }}
+                    />
+                </View>
+                <WebView
+                    scalesPageToFit={true}
+                    source={{ uri: path }}
+                    style={{ marginTop: 20, marginBottom: 20, }}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  titleText: {
-        fontSize: 40,
-        fontWeight: "bold",
-        color: "white",
-        marginRight: -10,
-        textAlign: 'center',
-       
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     droidSafeArea: {
         marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
     },
-    background: {
-      backgroundColor: '#1f0024'
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
     },
-    input: {
-        borderRadius: 100,
-        top: 0,
-        height: 40, 
-        borderColor: 'gray', 
+    titleText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "white",
+        justifyContent: "center",
+        alignContent: "center",
+    },
+    inputStyle: {
+        height: 40,
+        borderColor: 'gray',
         borderWidth: 1,
-        justifyContent: 'center',
+        borderRadius: 20,
+        marginTop: 20,
+        marginRight: 20,
+        marginLeft: 20,
         textAlign: 'center',
-        color: 'white'
-    },
-
+        color: 'white',
+        width: 200
+    }
 })
-
